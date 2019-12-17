46b58f758f0ec834bbf129b5ea51ab8f2fb706c8
arnolda <arnold.a@canva.com>
2019-12-17 19:26:32 +1100

    hand finished all methods

--- src/hand_utils.py
+++ src/hand_utils.py
@@ -1,4 +1,6 @@
 from deck import Deck, CardValue, CardSuit, Card
+import math
+
 
 def computeWinner():
     deck = Deck()
@@ -8,11 +10,7 @@ def computeWinner():
 
 # Returns a score associated with the hand type
 def isStraightFlush(sortedCards):
-    # Check for potential royal and swap sorted position
-    if sortedCards[0].value is CardValue.Ace and sortedCards[1].value is CardValue.Ten:
-        tmp = sortedCards[0]
-        sortedCards = sortedCards[1:]
-        sortedCards.append(Card(CardValue.High_ace, tmp.suit))
+    sortedCards = swapPotentialStraightAce(sortedCards)
 
     initialValue = sortedCards[0].value
     desired_suit = sortedCards[0].suit
@@ -26,18 +24,8 @@ def isStraightFlush(sortedCards):
 
     return straight_flush_value
 
-def createCountDict(sortedCards):
-    countDict = {}
-
-    for card in sortedCards:
-        if card.value not in countDict:
-            countDict[card.value] = 1
-        else:
-            countDict[card.value] += 1
-
-    return countDict
-
-def isQuads(sortedCards, countDict):
+def isQuads(sortedCards):
+    countDict = createCountDict(sortedCards)
     if (len(countDict) is not 2):
         return -1
 
@@ -55,7 +43,8 @@ def isQuads(sortedCards, countDict):
 
     return 100 * quad_value + kicker
 
-def isFullHouse(sortedCards, countDict):
+def isFullHouse(sortedCards):
+    countDict = createCountDict(sortedCards)
     if (len(countDict) is not 2):
         return -1
 
@@ -74,16 +63,111 @@ def isFullHouse(sortedCards, countDict):
     return 100 * boat_value + kicker
 
 def isFlush(sortedCards):
-    pass
+    desired_suit = sortedCards[0].suit
+    flush_value = computeValue(sortedCards[0])
+    for card in sortedCards[1:]:
+        if card.suit != desired_suit:
+            return -1
+
+        flush_value += computeValue(card)
+    return flush_value
 
 def isStraight(sortedCards):
-    pass
+    sortedCards = swapPotentialStraightAce(sortedCards)
+
+    initialValue = sortedCards[0].value
+    straight_value = initialValue
+    for card in sortedCards[1:]:
+        if card.value != initialValue + 1:
+            return -1
+
+        initialValue += 1
+        straight_value += card.value
+
+    return straight_value
+
+def isTrips(sortedCards):
+    countDict = createCountDict(sortedCards)
+    if (len(countDict) is not 3):
+        return -1
+
+    keys = list(countDict.keys())
+
+    trips = []
+    kickers = []
+    for key in keys:
+        if countDict[key] == 3:
+            trips.append(key)
+        else:
+            kickers.append(key)
+
+    if len(trips) is not 1 or len(kickers) is not 2:
+        return -1
+
+    trips_value = 0
+    for kicker in kickers:
+        trips_value += computeValue(kicker)
+
+    return trips_value + 15 ** trips[0].value
+
 
 def isTwoPair(sortedCards):
-    pass
+    countDict = createCountDict(sortedCards)
+    if (len(countDict) is not 3):
+        return -1
+
+    keys = list(countDict.keys())
+
+    pairs = []
+    kicker = 0
+    for key in keys:
+        if countDict[key] == 2:
+            pairs.append(key)
+        else:
+            kicker = key.value
+
+    if len(pairs) is not 2:
+        return -1
+
+    two_pair_value = 0
+    for pair in pairs:
+        two_pair_value += computeValue(pair)
+
+    return two_pair_value + kicker
 
 def isHighCard(sortedCards):
-    pass
+    countDict = createCountDict(sortedCards)
+    if (len(countDict) is not 5):
+        return -1
+
+    high_card_value = 0
+    for card in sortedCards:
+        high_card_value += computeValue(card)
+
+    return high_card_value
+
+def createCountDict(sortedCards):
+    countDict = {}
+
+    for card in sortedCards:
+        if card.value not in countDict:
+            countDict[card.value] = 1
+        else:
+            countDict[card.value] += 1
+
+    return countDict
+
+def swapPotentialStraightAce(sortedCards):
+    if sortedCards[0].value is CardValue.Ace and sortedCards[1].value is CardValue.Ten:
+        tmp = sortedCards[0]
+        sortedCards = sortedCards[1:]
+        sortedCards.append(Card(CardValue.High_ace, tmp.suit))
+    return sortedCards
+
+def computeValue(card):
+    if card.value is CardValue.Ace:
+        return 2 ** CardValue.High_ace
+    return 2 ** card.value
 
 # Takes in 5 card hand as input
 def determineBestHand(playableCards):
--- src/hand_utils.test.py
+++ src/hand_utils.test.py
@@ -1,7 +1,7 @@
 import unittest
 
 from deck import Deck, Card, CardValue, CardSuit
-from hand_utils import isStraightFlush, sortCards, isQuads, isFullHouse,  createCountDict
+from hand_utils import isStraightFlush, sortCards, isQuads, isFullHouse,  createCountDict, isFlush, isStraight,isTrips, isTwoPair, isHighCard
 
 class TestHandUtils(unittest.TestCase):
 
@@ -47,6 +47,48 @@ class TestHandUtils(unittest.TestCase):
         self.assertGreater(second_nut_flush, wheel_flush)
         self.assertGreater(royal, second_nut_flush)
 
+    def test_straight(self):
+        invalid_set = [
+                Card(CardValue.Ace, CardSuit.Club),
+                Card(CardValue.Deuce, CardSuit.Club),
+                Card(CardValue.Three, CardSuit.Heart),
+                Card(CardValue.Four, CardSuit.Club),
+                Card(CardValue.Six, CardSuit.Club)
+                ]
+        wheel_set = [
+                Card(CardValue.Ace, CardSuit.Club),
+                Card(CardValue.Deuce, CardSuit.Club),
+                Card(CardValue.Three, CardSuit.Club),
+                Card(CardValue.Four, CardSuit.Club),
+                Card(CardValue.Five, CardSuit.Club)
+                ]
+
+        royal_set = [
+                Card(CardValue.Ace, CardSuit.Club),
+                Card(CardValue.King, CardSuit.Club),
+                Card(CardValue.Queen, CardSuit.Club),
+                Card(CardValue.Jack, CardSuit.Club),
+                Card(CardValue.Ten, CardSuit.Club)
+                ]
+
+        second_nut_set = [
+                Card(CardValue.Nine, CardSuit.Club),
+                Card(CardValue.King, CardSuit.Club),
+                Card(CardValue.Queen, CardSuit.Club),
+                Card(CardValue.Jack, CardSuit.Club),
+                Card(CardValue.Ten, CardSuit.Club)
+                ]
+
+        invalid_result = isStraight(sortCards(invalid_set))
+        self.assertEqual(invalid_result, -1)
+
+        royal = isStraight(sortCards(royal_set))
+        wheel_flush = isStraight(sortCards(wheel_set))
+        second_nut_flush = isStraight(sortCards(second_nut_set))
+
+        self.assertGreater(second_nut_flush, wheel_flush)
+        self.assertGreater(royal, second_nut_flush)
+
     def test_quads(self):
         invalid_set = [
                 Card(CardValue.Ace, CardSuit.Club),
@@ -71,16 +113,16 @@ class TestHandUtils(unittest.TestCase):
                 Card(CardValue.Ace, CardSuit.Diamond),
                 Card(CardValue.King, CardSuit.Club)
                 ]
-        valid_set_result = isQuads(sortCards(valid_set), createCountDict(valid_set))
+        valid_set_result = isQuads(sortCards(valid_set))
 
         self.assertGreater(valid_set_result, 0)
 
-        invalid_result = isQuads(sortCards(invalid_set), createCountDict(invalid_set))
+        invalid_result = isQuads(sortCards(invalid_set))
 
         self.assertEqual(invalid_result, -1)
 
 
-        valid_set_nuts_result = isQuads(sortCards(valid_set_nuts), createCountDict(valid_set_nuts))
+        valid_set_nuts_result = isQuads(sortCards(valid_set_nuts))
 
 
         self.assertGreater(valid_set_nuts_result, valid_set_result)
@@ -109,30 +151,172 @@ class TestHandUtils(unittest.TestCase):
                 Card(CardValue.King, CardSuit.Diamond),
                 Card(CardValue.King, CardSuit.Club)
                 ]
-        valid_set_result = isFullHouse(sortCards(valid_set), createCountDict(valid_set))
+        valid_set_result = isFullHouse(sortCards(valid_set))
 
         self.assertGreater(valid_set_result, 0)
 
-        invalid_result = isFullHouse(sortCards(invalid_set), createCountDict(invalid_set))
+        invalid_result = isFullHouse(sortCards(invalid_set))
 
         self.assertEqual(invalid_result, -1)
 
 
-        valid_set_nuts_result = isFullHouse(sortCards(valid_set_nuts), createCountDict(valid_set_nuts))
+        valid_set_nuts_result = isFullHouse(sortCards(valid_set_nuts))
 
 
         self.assertGreater(valid_set_nuts_result, valid_set_result)
 
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
+    def test_flush(self):
+        invalid_set = [
+                Card(CardValue.Ace, CardSuit.Club),
+                Card(CardValue.Ace, CardSuit.Spade),
+                Card(CardValue.Ace, CardSuit.Heart),
+                Card(CardValue.King, CardSuit.Diamond),
+                Card(CardValue.Five, CardSuit.Club)
+                ]
 
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
+        valid_set = [
+                Card(CardValue.King, CardSuit.Club),
+                Card(CardValue.Queen, CardSuit.Club),
+                Card(CardValue.Jack, CardSuit.Club),
+                Card(CardValue.Ten, CardSuit.Club),
+                Card(CardValue.Eight, CardSuit.Club)
+                ]
+
+        valid_set_nuts = [
+                Card(CardValue.Ace, CardSuit.Club),
+                Card(CardValue.Deuce, CardSuit.Club),
+                Card(CardValue.Three, CardSuit.Club),
+                Card(CardValue.Four, CardSuit.Club),
+                Card(CardValue.Five, CardSuit.Club)
+                ]
+        valid_set_result = isFlush(sortCards(valid_set))
+
+        self.assertGreater(valid_set_result, 0)
+
+        invalid_result = isFlush(sortCards(invalid_set))
+
+        self.assertEqual(invalid_result, -1)
+
+
+        valid_set_nuts_result = isFlush(sortCards(valid_set_nuts))
+
+
+        self.assertGreater(valid_set_nuts_result, valid_set_result)
+
+    def test_trips(self):
+        invalid_set = [
+                Card(CardValue.Ace, CardSuit.Club),
+                Card(CardValue.Ace, CardSuit.Spade),
+                Card(CardValue.Ace, CardSuit.Heart),
+                Card(CardValue.King, CardSuit.Diamond),
+                Card(CardValue.King, CardSuit.Club)
+                ]
+
+        valid_set = [
+                Card(CardValue.Ace, CardSuit.Club),
+                Card(CardValue.Ace, CardSuit.Spade),
+                Card(CardValue.Ace, CardSuit.Heart),
+                Card(CardValue.Queen, CardSuit.Diamond),
+                Card(CardValue.Jack, CardSuit.Club)
+                ]
+
+        valid_set_nuts = [
+                Card(CardValue.Ace, CardSuit.Club),
+                Card(CardValue.Ace, CardSuit.Spade),
+                Card(CardValue.Ace, CardSuit.Heart),
+                Card(CardValue.Deuce, CardSuit.Diamond),
+                Card(CardValue.King, CardSuit.Club)
+                ]
+        valid_set_result = isTrips(sortCards(valid_set))
+
+        self.assertGreater(valid_set_result, 0)
+
+        invalid_result = isTrips(sortCards(invalid_set))
+
+        self.assertEqual(invalid_result, -1)
+
+
+        valid_set_nuts_result = isTrips(sortCards(valid_set_nuts))
+
+
+        self.assertGreater(valid_set_nuts_result, valid_set_result)
+
+    def test_two_pair(self):
+        invalid_set = [
+                Card(CardValue.Ace, CardSuit.Club),
+                Card(CardValue.Ace, CardSuit.Spade),
+                Card(CardValue.Ace, CardSuit.Heart),
+                Card(CardValue.King, CardSuit.Diamond),
+                Card(CardValue.King, CardSuit.Club)
+                ]
+
+        valid_set = [
+                Card(CardValue.Ace, CardSuit.Club),
+                Card(CardValue.Ace, CardSuit.Spade),
+                Card(CardValue.Deuce, CardSuit.Heart),
+                Card(CardValue.Deuce, CardSuit.Diamond),
+                Card(CardValue.Jack, CardSuit.Club)
+                ]
+
+        valid_set_nuts = [
+                Card(CardValue.Ace, CardSuit.Club),
+                Card(CardValue.Ace, CardSuit.Spade),
+                Card(CardValue.Deuce, CardSuit.Heart),
+                Card(CardValue.Deuce, CardSuit.Diamond),
+                Card(CardValue.King, CardSuit.Club)
+                ]
+        valid_set_result = isTwoPair(sortCards(valid_set))
+
+        self.assertGreater(valid_set_result, 0)
+
+        invalid_result = isTwoPair(sortCards(invalid_set))
+
+        self.assertEqual(invalid_result, -1)
+
+
+        valid_set_nuts_result = isTwoPair(sortCards(valid_set_nuts))
+
+
+        self.assertGreater(valid_set_nuts_result, valid_set_result)
+
+    def test_high_card(self):
+        invalid_set = [
+                Card(CardValue.Ace, CardSuit.Club),
+                Card(CardValue.Ace, CardSuit.Spade),
+                Card(CardValue.Ace, CardSuit.Heart),
+                Card(CardValue.King, CardSuit.Diamond),
+                Card(CardValue.King, CardSuit.Club)
+                ]
+
+        valid_set = [
+                Card(CardValue.Queen, CardSuit.Club),
+                Card(CardValue.King, CardSuit.Spade),
+                Card(CardValue.Deuce, CardSuit.Heart),
+                Card(CardValue.Three, CardSuit.Diamond),
+                Card(CardValue.Ten, CardSuit.Club)
+                ]
+
+        valid_set_nuts = [
+                Card(CardValue.Ace, CardSuit.Club),
+                Card(CardValue.King, CardSuit.Spade),
+                Card(CardValue.Deuce, CardSuit.Heart),
+                Card(CardValue.Three, CardSuit.Diamond),
+                Card(CardValue.Ten, CardSuit.Club)
+                ]
+        valid_set_result = isHighCard(sortCards(valid_set))
+
+        self.assertGreater(valid_set_result, 0)
+
+        invalid_result = isHighCard(sortCards(invalid_set))
+
+        self.assertEqual(invalid_result, -1)
+
+
+        valid_set_nuts_result = isHighCard(sortCards(valid_set_nuts))
+
+
+        self.assertGreater(valid_set_nuts_result, valid_set_result)
+
 
 if __name__ == '__main__':
     unittest.main()
